ac43b2f837cbbc05f72ed29a90654e8002f83a60
RevenSanchez <161605301+RevenSanchez@users.noreply.github.com>
2024-03-20 21:34:07 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,10 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+// 创建一个新的 meta 元素
+var metaTag = document.createElement('meta');
+
+// 设置 meta 标签的属性
+metaTag.setAttribute('name', 'baidu-site-verification');
+metaTag.setAttribute('content', 'codeva-UPfKcrCzI3');
+
+// 将 meta 元素添加到文档的 head 部分
+document.head.appendChild(metaTag);
